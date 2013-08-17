f0fdf1647c4dec770cf99887ccbc8e545e876303
David A. Rogers <darogers@mighty.local>
2013-08-16 20:13:40 -0500

    Created third controller.

---
+++ SimpleLayoutDemo/SLDThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLDThirdViewController.h
+//  SimpleLayoutDemo
+//
+//  Created by David A. Rogers on 8/16/13.
+//  Copyright (c) 2013 David Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLDThirdViewController : UIViewController
+
+@end
---
+++ SimpleLayoutDemo/SLDThirdViewController.m
@@ -0,0 +1,49 @@
+//
+//  SLDThirdViewController.m
+//  SimpleLayoutDemo
+//
+//  Created by David A. Rogers on 8/16/13.
+//  Copyright (c) 2013 David Rogers. All rights reserved.
+//
+
+#import "SLDThirdViewController.h"
+
+@interface SLDThirdViewController ()
+
+@end
+
+@implementation SLDThirdViewController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.title = NSLocalizedString(@"Second", @"Second");
+        self.tabBarItem.image = [UIImage imageNamed:@"second"];
+    }
+    return self;
+}
+
+-(void)loadView {
+    UIView* newView = [[[UIView alloc] init] autorelease];
+    newView.backgroundColor = [UIColor greenColor];
+    self.view = newView;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    NSLog(@"view frame is %@", NSStringFromCGRect(self.view.frame));
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
